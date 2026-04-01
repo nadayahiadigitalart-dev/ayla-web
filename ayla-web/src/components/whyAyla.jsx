236c861cfa3577ec43fc755a0e53980a6bfbaddd
Nadayahia_dev <nadayahiadigitalart-dev>
2026-04-01 12:55:07 +0200

    why ayla section done

--- ayla-web/src/components/whyAyla.jsx
+++ ayla-web/src/components/whyAyla.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import '.WhyAyla.css';
+import './WhyAyla.css';
+import Button from './Button';
+import icon1 from "../assets/icon1.svg";
+import icon2 from "../assets/icon2.svg";
+import icon3 from "../assets/icon3.svg";
+import icon4 from "../assets/icon4.svg";
 
-const WhyAyla = () => {
+
+
+const WhyAyla = (props) => {
     const cards = [
         { title: "Reminders", desc: "Track yourself and your baby vaccines and health", icon: "🔔" },
         { title: "Parenting Advice", desc: "Learn to understand your child and parent him perfectly...", icon: "❤️" },
@@ -13,15 +20,51 @@ const WhyAyla = () => {
         <section className="ayla_section">
             <h2 className="ayla_title">Why ayla?</h2>
             <div className="cards_container">
-                {cards.map((card, index) => (
+                {/* {cards.map((card, index) => (
                     <div className="ayla_card" key={index}>
                         <span className="card_icon">{card.icon}</span>
                         <h3>{card.title}</h3>
                         <p>{card.desc}</p>
                     </div>
-                ))}
+                ))} */}
+
+                <div className='ayla_card'>
+                    <img src={icon1} alt="Reminder icon"/>
+                    <p className='pf' >Reminder</p>
+                    <p className='bf' >Track yourself and your baby vaccines and health </p>
+
+                </div>
+
+                 <div className='ayla_card2'>
+                    <img src={icon2} alt="Parenting Advice"/>
+                    <p className='pf' >Parenting Advice</p>
+                    <p className='bf' >Learn to understand your 
+                        child and parent him perfectly in every 
+                        phase they go to and learn how to deal 
+                        with real situations. </p>
+
+                </div>
+
+                 <div className='ayla_card3'>
+                    <img src={icon3} alt="community"/>
+                    <p className='pf' >Community</p>
+                    <p className='bf' >Share experiences,
+                         discuss with other mothers and see 
+                         other opinions and problems and 
+                         encourage each other.</p>
+
+                </div>
+
+                 <div className='ayla_card4'>
+                    <img src={icon4} alt="ask experts icon"/>
+                    <p className='pf' >Ask Experts</p>
+                    <p className='bf' >Connect with experts, psychologists 
+                        and experts to understand your children and 
+                        raise them carefully.</p>
+
+                </div>
             </div>
-            <button className="see_more_btn">See more</button>
+            <Button bu="See more" />
         </section>
     );
 };
--- ayla-web/src/pages/Home.jsx
+++ ayla-web/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Hero from '../components/Hero';
 import Header from '../components/Header';
-import bg from "../assets/bg-img.svg";
+// import bg from "../assets/bg-img.svg";
+// import bg from "../assets/bg1-img.svg";
+import parent from "../assets/parent.svg";
+import G189 from "../assets/Group 189.png";
 
 import "./Home.css";
 import WhyAyla from '../components/whyAyla';
+import Button from '../components/Button';
 
 
 
@@ -13,19 +17,29 @@ const Home = () => {
 
     <div className='bg'>
 
-    <img className='bgsize' src={bg} alt="ayla_background" />
+    {/* <img className='bgsize' src={bg} alt="ayla_background" /> */}
 
 
 <div className='header'>
 
-    <Header />
 </div>
 
-{/* <div className='absolute_bg'> */}
+<div className='absolute_bg'>
+    <Header />
     <Hero />
-{/* </div> */}
 
 <WhyAyla />
+</div>
+
+<div className='parenting'>
+    <img className='absolute_parent' src={G189} alt='parenting image'/>
+    <div className='parent_col'>
+    <p className='mf'>Ever wondered how to deal with difficult parent decisions?</p>
+    <p className='sf'>Discover what your child is experiencing and learn how to support them with confidence.</p>
+    <p className='f'>From baby to teenage years, Ayla helps parents understand how children think, feel, and behave with guidance from child development experts and how to behave in difficult parenting situations</p>
+    <Button bu="Explore each stage"/>
+    </div>
+</div>
 
 
 
